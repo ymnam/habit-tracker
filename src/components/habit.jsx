46155be07c1d886f0517f9005f2f550bb3e88235
ymnam <ymnam@wavus.co.kr>
2020-09-28 16:55:22 +0900

    4.8 보충설명 (Props에 대해서)

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -19,10 +19,12 @@ class Habit extends Component {
   };
 
   render() {
+    console.log(this.props.habit);
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncrement}
