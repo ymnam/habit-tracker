941e37c16bf5a1633e0584da01d0642f392f5fb9
ymnam <ymnam@wavus.co.kr>
2020-09-28 16:19:14 +0900

    4.6 Habit 컴포넌트 만들기, 4.7 state 이해하기

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
-  // const name = 'ellie';
-  const name = undefined;
-  return (
-    <>
-      <h1>Hello</h1>
-      {name && <h1>Hello {name}:)</h1>}
-      {['❤', '🍎'].map((item) => (
-        <h1>{item}</h1>
-      ))}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
