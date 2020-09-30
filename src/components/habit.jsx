e8d0370eb2b4fbdac74e31226a31ece57914b4f5
ymnam <hanist92@gmail.com>
2020-10-01 00:29:10 +0900

    4.9 이벤트 처리하기 (state 업데이트)

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -6,20 +6,19 @@ class Habit extends Component {
   };
 
   handleIncrement = () => {
-    // handleIncrement = (event) => {
-    //   console.log(event);
-    // }
-    // state 오브젝트 안에 있는 count를 증가한뒤 state를 업데이트 해야 함.
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
-    console.log(this.props.habit);
+    // console.log(this.props.habit);
     const { name, count } = this.props.habit;
     return (
       <li className="habit">
@@ -37,7 +36,10 @@ class Habit extends Component {
         >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
